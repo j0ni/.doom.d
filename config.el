ce743122c91296d2f76ff271e56afbc738a98470
Jon Irving <j@lollyshouse.ca>
2020-10-12 21:16:51 +0200

    Add back missing typescript setup

--- config.el
+++ config.el
@@ -93,7 +93,13 @@
     )))
 
 (defun setup-tide-mode ()
-  )
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
 
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 (add-hook 'before-save-hook #'tide-format-before-save)
